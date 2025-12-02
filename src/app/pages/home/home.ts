6d7ca5299fba1d392ea2ecadb080364dbff6a045
Harita Dutt <haritavideos.1@gmail.com>
2025-12-02 20:14:23 +0530

    Add projects component

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Projects } from '../projects/projects';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterModule,CommonModule],
+  imports: [RouterModule,CommonModule,Projects],
   standalone: true,
   templateUrl: './home.html',
   styleUrl: './home.scss',
